12e6612eb9e68d5659f8ae38d5e7df3f9517b121
ByeongSu Do <bsdo@naver.com>
2016-07-15 15:52:23 +0900

    fix noti socket

--- Clients/NotiSocketClient/index.js
+++ Clients/NotiSocketClient/index.js
@@ -1,4 +1,10 @@
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3003/noti', {reconnect: true});
+
+let socket;
+if (process.env.NODE_ENV === 'production') {
+  socket = io.connect('http://io.venacle.com/noti');
+} else {
+  socket = io.connect('http://localhost:3003/noti');
+}
 
 module.exports = socket;

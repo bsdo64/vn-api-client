5868d56889141d03bc3176af6d65457887389158
ByeongSu Do <bsdo@naver.com>
2016-11-07 19:08:38 +0900

    add veanlink socket

--- Clients/SocketClient/index.js
+++ Clients/SocketClient/index.js
@@ -1,15 +1,12 @@
 const io = require('socket.io-client');
 
 let manager;
-let NotiSocket, PointSocket;
 if (process.env.NODE_ENV === 'production') {
   manager = io.Manager('http://io.venacle.com');
 } else {
   manager = io.Manager('http://localhost:3003');
 }
 
-NotiSocket = manager.socket('/noti', {reconnect: true});
-PointSocket = manager.socket('/point', {reconnect: true});
-
-exports.Point = PointSocket;
-exports.Noti = NotiSocket;
+exports.Point = manager.socket('/noti', {reconnect: true});
+exports.Noti = manager.socket('/point', {reconnect: true});
+exports.Venalink = manager.socket('/venalink', {reconnect: true});
